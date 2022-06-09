0107ae02bf2af540d55b8b42b45501eb2824fac4
David-Lor <17401854+David-Lor@users.noreply.github.com>
2022-06-09 23:27:12 +0200

    Basic logic for retrying image generation until success

--- dalle_telegram_bot/services/bot/bot.py
+++ dalle_telegram_bot/services/bot/bot.py
@@ -1,8 +1,13 @@
+from time import sleep
+from threading import Thread, Event
+from typing import Dict, Optional
+
 import telebot
 from telebot.types import Message, InputMediaPhoto
 
 from .constants import BasicCommandsReplies, CommandGenerate
 from ..dalle import Dalle, DalleTemporarilyUnavailableException
+from ..dalle.models import DalleResponse
 from ...settings import Settings
 
 
@@ -10,11 +15,14 @@ class Bot:
     def __init__(self, settings: Settings, dalle: Dalle):
         self._settings = settings
         self._dalle = dalle
+
         self._bot = telebot.TeleBot(
             token=self._settings.telegram_bot_token,
             parse_mode="HTML"
         )
         self._bot.message_handler(func=lambda message: True)(self._handler_message_entrypoint)
+
+        self._typingaction_chatids_events: Dict[int, Event] = dict()
 
     def run(self):
         self._bot.infinity_polling()
@@ -43,16 +51,24 @@ class Bot:
             # TODO Error message
             return True
 
-        try:
-            response = self._dalle.generate(
-                prompt=prompt,
-            )
-        # TODO Error messages
-        except DalleTemporarilyUnavailableException:
-            self._bot.reply_to(message, "Service temporarily unavailable")
-            return True
-        except Exception:
-            self._bot.reply_to(message, "Unknown error")
+        response: Optional[DalleResponse] = None
+        self.start_typing_action(message.chat.id)
+
+        while True:
+            try:
+                response = self._dalle.generate(
+                    prompt=prompt,
+                )
+            except DalleTemporarilyUnavailableException:
+                sleep(5)
+            except Exception:
+                self._bot.reply_to(message, "Unknown error")
+                break
+            else:
+                break
+
+        self.stop_typing_action(message.chat.id)
+        if not response:
             return True
 
         images_telegram = [InputMediaPhoto(image_bytes) for image_bytes in response.images_bytes]
@@ -61,3 +77,32 @@ class Bot:
             reply_to_message_id=message.message_id,
             media=images_telegram,
         )
+        return True
+
+    def start_typing_action(self, chat_id: int):
+        # TODO Compatibilize when a chat_id sends multiple requests
+        event = Event()
+        thread = Thread(
+            target=self._typing_action_worker,
+            kwargs=dict(
+                chat_id=chat_id,
+                event=event,
+            ),
+            name=f"TypingActionWorker-{chat_id}",
+            daemon=True
+        )
+        self._typingaction_chatids_events[chat_id] = event
+        thread.start()
+
+    def stop_typing_action(self, chat_id: int):
+        event = self._typingaction_chatids_events.get(chat_id)
+        if event:
+            event.set()
+
+    def _typing_action_worker(self, chat_id: int, event: Event):
+        while not event.is_set():
+            self._bot.send_chat_action(
+                chat_id=chat_id,
+                action="typing",
+            )
+            event.wait(4.5)
