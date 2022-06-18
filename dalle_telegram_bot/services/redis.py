4ed9bdb90140e2a80327a042c0b132ea8ca10513
David-Lor <17401854+David-Lor@users.noreply.github.com>
2022-06-19 01:04:18 +0200

    internal: ignore errors on log push to Redis

--- dalle_telegram_bot/services/redis.py
+++ dalle_telegram_bot/services/redis.py
@@ -23,7 +23,11 @@ class Redis(AbstractLogger):
         if not self._redis or not self._settings.redis_logs_queue_name:
             return
 
-        self._redis.rpush(
-            self._settings.redis_logs_queue_name,
-            data,
-        )
+        try:
+            self._redis.rpush(
+                self._settings.redis_logs_queue_name,
+                data,
+            )
+        except Exception:
+            # TODO Log errors?
+            pass
